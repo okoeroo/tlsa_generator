36fc23b49400640377d49ed55d48945e891905da
Oscar Koeroo <okoeroo@gmail.com>
2018-09-30 13:34:40 +0200

    Fix. it did not work.

--- gen_tlsa.sh
+++ gen_tlsa.sh
@@ -13,22 +13,31 @@ function run() {
     FQDN=$1
     PORT=$2
 
-    HASH=$(echo | \
-        if [ $PORT -eq 25 ]; then
-            OUTPUT=$(timeout 5 openssl s_client -connect ${FQDN}:${PORT} -servername ${FQDN} -starttls smtp 2>/dev/null)
-        else
-            OUTPUT=$(timeout 5 openssl s_client -connect ${FQDN}:${PORT} -servername ${FQDN} 2>/dev/null)
-        fi
-        RC=$?
-        if [ $RC -ne 0 ]; then
-            echo failed
-            return $RC
-        fi
+    if [ $PORT -eq 25 ]; then
+        HASH=$(echo | \
+            timeout 5 openssl s_client -connect ${FQDN}:${PORT} -servername ${FQDN} -starttls smtp 2>/dev/null | \
+                openssl x509 -pubkey -noout 2>/dev/null | \
+                openssl rsa -pubin -outform DER 2>/dev/null | \
+                ${SHA256} | \
+                tr "a-z" "A-Z" | cut -d" " -f 1)
+    else
+        HASH=$(echo | \
+            timeout 5 openssl s_client -connect ${FQDN}:${PORT} -servername ${FQDN} 2>/dev/null | \
+                openssl x509 -pubkey -noout 2>/dev/null | \
+                openssl rsa -pubin -outform DER 2>/dev/null | \
+                ${SHA256} | \
+                tr "a-z" "A-Z" | cut -d" " -f 1)
+    fi
+    RC=$?
+    if [ $RC -ne 0 ]; then
+        echo failed
+        return $RC
+    fi
 
-        echo $OUTPUT | openssl x509 -pubkey -noout 2>/dev/null | \
-        openssl rsa -pubin -outform DER 2>/dev/null | \
-        ${SHA256} | \
-        tr "a-z" "A-Z" | cut -d" " -f 1)
+    # Error
+    if [ $HASH = "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855" ]; then
+        HASH="failed"
+    fi
 
     echo "_${PORT}._tcp.${FQDN}.     IN TLSA     3 1 1 $HASH"
 }
